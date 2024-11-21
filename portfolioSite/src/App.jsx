41ff798eb10997b2b19bed56f1ffe3112a2a1dc2
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 15:25:03 -0800

    added sampleLogo

--- portfolioSite/src/App.jsx
+++ portfolioSite/src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden text-stone-300 antialiased'>
-      <div className='fixed inset-0 -z-10'>
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className="overflow-x-hidden text-stone-300 antialiased">
+      <div className="fixed inset-0 -z-10">
+        <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- portfolioSite/src/components/Navbar.jsx
+++ portfolioSite/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import logo from "../assets/sampleLogo.webp"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <a href="/" aria-label="Home">
+          <img src={logo} className=''/>
+        </a>
+      </div>
+    </nav>
   )
 }
 
