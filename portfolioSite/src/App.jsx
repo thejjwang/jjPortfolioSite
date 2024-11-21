70ae6ee819b2d4358354f4998a00aa7f7f8d97af
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 14:43:39 -0800

    all plugins running smoothly

---
+++ portfolioSite/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="bg-blue-500 text-white">
+  This should have a blue background and white text.
+</div>
+  )
+}
+
+export default App
