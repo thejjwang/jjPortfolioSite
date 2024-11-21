0f476edd8b877d35fdb30465a199e8af8791af0f
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 15:47:30 -0800

    added github, instagram, facebook

--- portfolioSite/src/components/Navbar.jsx
+++ portfolioSite/src/components/Navbar.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
-import logo from '../assets/Logo2.png';
-import { FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import logo from "../assets/Logo.png";
+import { FaLinkedin, FaInstagram, FaGithub, FaFacebook } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between py-6'>
-      <div className='flex flex-shrink-0 items-center'>
-        <a href='/' aria-label='Home'>
-          <img src={logo} className='mx-2' width={50} height={33} alt='Logo'/>
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
         </a>
       </div>
-      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <a href='https://www.linkedin.com/in/jjwang2022/' target='_blank' rel='noopener noreferrer' aria-label='LinkedIn'>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/jjwang2022/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
           <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/thejjwang"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/jj.wang/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://www.facebook.com/jj.wang.33671/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Facebook"
+        >
+          <FaFacebook />
         </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
