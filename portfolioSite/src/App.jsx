a5f610995a9c054fd9c9c1060c38c72c4807e9c3
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 15:03:31 -0800

    imported resume/logo/profile etc

--- portfolioSite/src/App.jsx
+++ portfolioSite/src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div class="bg-blue-500 text-white">
-  This should have a blue background and white text.
-</div>
+  hello world
+    </div>
   )
 }
 
