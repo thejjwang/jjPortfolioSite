ee2a90e341301170e75d06a5b526429bbbb7bfb5
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 15:30:31 -0800

    installed react icons and added linkedin button

--- portfolioSite/src/components/Navbar.jsx
+++ portfolioSite/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import logo from "../assets/sampleLogo.webp"
+import logo from '../assets/sampleLogo.webp';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className='flex items-center justify-between py-6'>
       <div className='flex flex-shrink-0 items-center'>
-        <a href="/" aria-label="Home">
-          <img src={logo} className=''/>
+        <a href='/' aria-label='Home'>
+          <img src={logo} className='mx-2' width={50} height={33} alt='Logo'/>
+        </a>
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <a href='https://www.linkedin.com/in/jjwang2022/' target='_blank' rel='noopener noreferrer' aria-label='LinkedIn'>
+          <FaLinkedin />
         </a>
       </div>
     </nav>
