174a15e12861215e28c3ca96bae6d85d64e66979
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 15:15:12 -0800

    changed background from bg.ibelick

--- portfolioSite/src/App.jsx
+++ portfolioSite/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div class="bg-blue-500 text-white">
-  hello world
+    <div className='overflow-x-hidden text-stone-300 antialiased'>
+      <div className='fixed inset-0 -z-10'>
+      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
     </div>
   )
 }
