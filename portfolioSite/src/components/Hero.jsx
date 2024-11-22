f8044c11786c68e47d63172ede8de782dc91d332
thejjwang <jonathanjacobwang@gmail.com>
2024-11-21 16:15:26 -0800

    download resume works

--- portfolioSite/src/components/Hero.jsx
+++ portfolioSite/src/components/Hero.jsx
@@ -16,8 +16,8 @@ const Hero = () => {
                 <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
                     {HERO_CONTENT}
                 </p>
-                <a>
-
+                <a href='/portfolioRes.pdf' target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
+                    Download Resume
                 </a>
             </div>
         </div>
