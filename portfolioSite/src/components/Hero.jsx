af36a3da6835f02a7d07e883605042a84143bcbd
thejjwang <jonathanjacobwang@gmail.com>
2024-11-26 12:22:52 -0800

    reformatted hero section

--- portfolioSite/src/components/Hero.jsx
+++ portfolioSite/src/components/Hero.jsx
@@ -1,28 +1,44 @@
-import profilePic from '../assets/sampleProfile.webp'
-import { HERO_CONTENT } from '../constants'
+import profilePic from "../assets/sampleProfile.webp";
+import { HERO_CONTENT } from "../constants";
 
 const Hero = () => {
   return (
-    <div className='pb-4 lg:mb-36'>
-        <div className='flex flex-wrap lg:flex-row-reverse'>
-            <div className='w-full lg:w-1/2'>
-                <img src={profilePic} alt='JJ Wang' className='border border-stone-900 rounded-3xl'/>
-            </div>
+    <div className="pb-4 lg:mb-36">
+      <div className="flex flex-wrap lg:flex-row-reverse">
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:p-8">
+            <img
+              src={profilePic}
+              alt="JJ Wang"
+              className="border border-stone-900 rounded-3xl"
+            />
+          </div>
         </div>
-        <div className='w-full lg:w-1/2'>
-            <div className='flex flex-col items-center lg:items-start mt-10'>
-                <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>JJ Wang</h2>
-                <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
-                <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
-                    {HERO_CONTENT}
-                </p>
-                <a href='/portfolioRes.pdf' target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
-                    Download Resume
-                </a>
-            </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start mt-10">
+            <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+              JJ Wang
+            </h2>
+            <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
+              Full Stack Developer
+            </span>
+            <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+            <a
+              href="/portfolioRes24.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
+            >
+              Download Resume
+            </a>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
