d75432fc452368f41b166773795c781ab584a5b6
thejjwang <jonathanjacobwang@gmail.com>
2024-12-02 12:41:24 -0800

    added project section

--- portfolioSite/src/components/Projects.jsx
+++ portfolioSite/src/components/Projects.jsx
@@ -1,7 +1,18 @@
-
+import { PROJECTS } from "../constants"
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='pb-4'>
+        <h2 className='my-20 text-center text-4xl'>Projects</h2>
+        <div>
+            {PROJECTS.map((project, index) => (
+                <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+                    <div className='w-full lg:w-1/4'>
+                        <img src={project.image} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
