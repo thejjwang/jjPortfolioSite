e6dfea4046ebe81d16904daa228a820769169cb4
thejjwang <jonathanjacobwang@gmail.com>
2024-12-05 10:36:33 -0800

    adding my contact

--- portfolioSite/src/App.jsx
+++ portfolioSite/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
