85a35bd0ac121015360e89f4af802a842e74bf65
thejjwang <jonathanjacobwang@gmail.com>
2024-12-05 12:27:43 -0800

    created iconVariants for technologies ection animation

--- portfolioSite/src/components/Technologies.jsx
+++ portfolioSite/src/components/Technologies.jsx
@@ -7,11 +7,28 @@ import { SiExpress } from "react-icons/si";
 import { TiHtml5 } from "react-icons/ti";
 import { FaAws } from "react-icons/fa";
 import { MdCss } from "react-icons/md";
+import { motion } from "framer-motion"
 
+const iconVariants = (duration) => ({
+  initial: { y: -10},
+  animate: {
+    y: [10,-10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+  }
+})
 const Technologies = () => {
   return (
     <div className="pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <motion.h2
+      whileInView={{ opacity: 1, y: 0}}
+      initial={{ opacity: 0, y: -100}}
+      transition={{ duration: 1.5 }}
+      className="my-20 text-center text-4xl">Technologies</motion.h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div>
           <RiReactjsLine className="text-7xl text-cyan-400" />
