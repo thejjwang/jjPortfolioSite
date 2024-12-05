3fc314ec82e4a2ef68605a329e768c2a3a9e5d2b
thejjwang <jonathanjacobwang@gmail.com>
2024-12-04 16:41:02 -0800

    experience section samples done

--- portfolioSite/src/components/Experience.jsx
+++ portfolioSite/src/components/Experience.jsx
@@ -5,19 +5,26 @@ const Experience = () => {
     <div className='pb-4'>
         <h2 className='my-20 text-center text-4xl'>Experience</h2>
         <div>
-            {EXPERIENCES.map((Experience, index) => (
+            {EXPERIENCES.map((experience, index) => (
                 <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
                     <div className='w-full lg:w-1/4'>
                         <p className='mb-2 text-sm text-stone-400'>
-                            {Experience.year}
+                            {experience.year}
                         </p>
                     </div>
-                    <div>
-                        <h3>
-                            (experience.role) - {" "}
-                            <span></span>
+                    <div className='w-full max-w-xl lg:w-3/4'>
+                        <h3 className='mb-2 font-semibold'>
+                            {experience.role} - {" "}
+                            <span className='text-sm text-stone-500'>
+                                {experience.company}
+                            </span>
                         </h3>
-                        <p></p>
+                        <p classname='mb-4 text-stone-400'>{experience.description}</p>
+                            {experience.technologies.map((tech, index) => {
+                                <span className='mr-2 mt-4 rounded bg-stone-300 px-2 py-1 text-sm font-medium text-stone-300' key={index}>
+                                    {tech}
+                                </span>
+                            })}
                     </div>
                 </div>
             ))}
