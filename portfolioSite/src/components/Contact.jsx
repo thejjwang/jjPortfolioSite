6501cdd9ad8d150b97655c9771374d5af3724ac2
thejjwang <jonathanjacobwang@gmail.com>
2024-12-09 11:00:34 -0800

    added framer motion to contact section

--- portfolioSite/src/components/Contact.jsx
+++ portfolioSite/src/components/Contact.jsx
@@ -1,14 +1,35 @@
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 const Contact = () => {
   return (
     <div className="border-t border-stone-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Get In Touch</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 1, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get In Touch
+      </motion.h2>
       <div className="text-center tracking-tighter">
         {/* <p className="my-4">{CONTACT.address}</p> */}
-        <p className="my-4">{CONTACT.phoneNo}</p>
-        <a href="#" className="border-b">
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <motion.a
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          href="#"
+          className="border-b"
+        >
           {CONTACT.email}
-        </a>
+        </motion.a>
       </div>
     </div>
   );
